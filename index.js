5b327370dac4805583f1034f05b05fc24413d41a
Sergiu Ghenciu <1024gs@gmail.com>
2019-10-21 19:03:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const isArray = (x) => Array.isArray(x);
 const isNull = (x) => x === null;
 
 const greet = (name) => {
-    if(isNull(name)) {
-		return `Hello my friend.`;
-    }
-	if(isArray(name) && name.length <= 2) {
-		return `Hello ${name.join(' and ')}.`;
-    }
-	if(isArray(name) && name.length > 2) {
-		return `Hello ${init(name).join(', ')}, and ${last(name)}.`;
-    }
-	if(isUpperCase(name)) {
-		return `HELLO ${name}.`;
-    }
+  if(isNull(name)) {
+    return `Hello my friend.`;
+  }
+  if(isArray(name) && name.length <= 2) {
+    return `Hello ${name.join(' and ')}.`;
+  }
+  if(isArray(name) && name.length > 2) {
+    return `Hello ${init(name).join(', ')}, and ${last(name)}.`;
+  }
+  if(isUpperCase(name)) {
+    return `HELLO ${name}.`;
+  }
 	
-	return `Hello ${name}.`;
+  return `Hello ${name}.`;
 };
